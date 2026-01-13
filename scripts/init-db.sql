873383713147cdebffc4f0151d5591d6641d4eea
pablo braga <pablo.gerheim@onecontrol.io>
2026-01-13 18:22:00 -0300

    limpeza

--- scripts/init-db.sql
+++
@@ -1,8 +0,0 @@
--- LEP System - Database Initialization Script
--- Extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON DATABASE lep_database TO lep_user;
-ALTER ROLE lep_user SET search_path TO public;
